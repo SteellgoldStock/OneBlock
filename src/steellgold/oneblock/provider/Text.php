e0c142cd96f67499c49e13d67d987477d28b373d
Gaëtan H <steellgold0@gmail.com>
2022-05-25 21:14:42 +0200

    add text "provider"

---
+++ src/steellgold/oneblock/provider/Text.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace steellgold\oneblock\provider;
+
+use steellgold\oneblock\One;
+
+class Text {
+	public static function getCommandDescription(string $command){
+		return One::getInstance()->getConfig()->get("commands")[$command] ?? "Description not found";
+	}
+
+	public static function getMessage(string $identifier){
+		return One::getInstance()->getConfig()->get("messages")[$identifier] ?? "Message not found";
+	}
+
+	public static function getPrefix(bool $error = false){
+		$prefix = One::getInstance()->getConfig()->get("messages")["prefix"];
+		return $error ? $prefix['error'] : $prefix['success'];
+	}
+}
