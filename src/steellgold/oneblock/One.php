44bc9372ed03f2b5af60b80aa0efc3a5ff849447
Gaëtan H <steellgold0@gmail.com>
2022-05-25 21:15:28 +0200

    add config save, tiers registrations and register island command

--- src/steellgold/oneblock/One.php
+++ src/steellgold/oneblock/One.php
@@ -2,14 +2,37 @@
 
 namespace steellgold\oneblock;
 
+use CortexPE\Commando\PacketHooker;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use steellgold\oneblock\commands\IslandCommand;
+use steellgold\oneblock\instances\Tier;
+use steellgold\oneblock\provider\Text;
 
 class One extends PluginBase {
+
+	public array $tiers = [];
 
 	public static $instance;
 
 	protected function onEnable(): void {
 		self::$instance = $this;
+
+		if (!file_exists($this->getDataFolder() . "config.yml")) {
+			$this->saveResource("config.yml");
+			$this->saveResource("islandConfig.yml");
+		}
+
+		if(!PacketHooker::isRegistered()) {
+			PacketHooker::register($this);
+		}
+
+		$islandConfig = new Config($this->getDataFolder() . "islandConfig.yml", Config::YAML);
+		foreach ($islandConfig->get("tiers") as $tierId => $tier){
+			$this->tiers[$tierId] = new Tier($tier["name"], $tier["breakToUp"], $tier["blocks"]);
+		}
+
+		$this->getServer()->getCommandMap()->register("oneblock", new IslandCommand($this, "island", Text::getCommandDescription("default"), ["is"]));
 	}
 
 	/**
--- src/steellgold/oneblock/instances/Tier.php
+++ src/steellgold/oneblock/instances/Tier.php
@@ -5,12 +5,14 @@ namespace steellgold\oneblock\instances;
 class Tier {
 
 	/**
+	 * @param string $name
 	 * @param int $breakToUp
 	 * @param array $blocks
 	 */
 	public function __construct(
+		public string $name,
 		public int $breakToUp,
-		public array $blocks,
+		public array $blocks
 	) {
 
 	}
